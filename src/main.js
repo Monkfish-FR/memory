ac18db38f8de1a8d06e5e29a674b2a71df06e4b2
Fabien TAVERNIER <contact@monkfish.fr>
2022-01-04 19:30:57 +0100

    Header: add scroll on touch

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import './scss/main.scss';
 
-console.log('WELCOME to main.js!');
+const $game = document.getElementById('game');
+const $headerScroll = document.getElementById('headerScroll');
+
+/* Défiler jusqu'à la div #game  */
+$headerScroll.addEventListener('touchend', () => {
+  $game.scrollIntoView({ behavior: 'smooth' });
+});
